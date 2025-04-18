34eee290c3f6540fe8be7ce2ab087fcfa8e5cb0e
smmrue <psjung0826@naver.com>
2025-04-18 18:12:27 +0900

    firebase 수정

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey-abc123.json"); // 여기에 실제 파일 이름!
+const serviceAccount = require("./firebase-adminsdk.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- frontend/src/api/firebase.js
+++ frontend/src/api/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBL5X7nXyc0ZR7uLWYCC-WE_NJ_BwzooEw",
+  authDomain: "triples2-1d0c7.firebaseapp.com",
+  projectId: "triples2-1d0c7",
+  appId: "1:329500842061:web:133bd12d72c7761ffd6404",
 };
 
 const app = initializeApp(firebaseConfig);
