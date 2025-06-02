0990867efe4b490e19ab7bed33e3dd69c1e377c8
KNH <nabing39701@naver.com>
2025-06-02 23:12:49 +0900

    icons

--- frontend/src/assets/icons/index.jsx
+++ frontend/src/assets/icons/index.jsx
@@ -1,10 +1,47 @@
-// src/assets/icons/index.js
+// src/assets/icons/index.js\
+
 import Home from "./Home";
+import Mail from "./Mail";
+import Lock from "./Lock";
+import User from "./User";
+import Heart from "./Heart";
+import Plus from "./Plus";
+import Search from "./Search";
+import Location from "./Location";
+import Call from "./Call";
+import Camera from "./Camera";
+import Edit from "./Edit";
+import ArrowLeft from "./ArrowLeft";
+import threeDotsCircle from "./threeDotsCircle";
+import threeDotsHorizontal from "./threeDotsHorizontal";
+import Comment from "./Comment";
+import Share from "./Share";
+import Delete from "./Delete";
+import logout from "./logout";
+import Image from "./Image";
 
 export const icons = {
   Home,
-};
+  Image,
+  Mail,
+  User,
+  Heart,
+  ArrowLeft,
+  Call,
+  Camera,
+  Comment,
+  Delete,
+  Edit,
+  Location,
+  Lock,
+  logout,
+  Plus,
+  Search,
 
+  Share,
+  threeDotsCircle,
+  threeDotsHorizontal,
+};
 // ✅ 이 줄 추가!
 const Icon = ({
   name,
