15598a7f42526b2b7068029aa465ed08793c4c90
KNH <nabing39701@naver.com>
2025-06-03 00:00:22 +0900

    icons

--- frontend/src/assets/icons/index.jsx
+++ frontend/src/assets/icons/index.jsx
@@ -1,47 +1,7 @@
 // src/assets/icons/index.js\
 
-import Home from "./Home";
-import Mail from "./Mail";
-import Lock from "./Lock";
-import User from "./User";
-import Heart from "./Heart";
-import Plus from "./Plus";
-import Search from "./Search";
-import Location from "./Location";
-import Call from "./Call";
-import Camera from "./Camera";
-import Edit from "./Edit";
-import ArrowLeft from "./ArrowLeft";
-import threeDotsCircle from "./threeDotsCircle";
-import threeDotsHorizontal from "./threeDotsHorizontal";
-import Comment from "./Comment";
-import Share from "./Share";
-import Delete from "./Delete";
-import logout from "./logout";
-import Image from "./Image";
+import { icons } from "./iconMap";
 
-export const icons = {
-  Home,
-  Image,
-  Mail,
-  User,
-  Heart,
-  ArrowLeft,
-  Call,
-  Camera,
-  Comment,
-  Delete,
-  Edit,
-  Location,
-  Lock,
-  logout,
-  Plus,
-  Search,
-
-  Share,
-  threeDotsCircle,
-  threeDotsHorizontal,
-};
 // ✅ 이 줄 추가!
 const Icon = ({
   name,
