6d7929dbeca5e1759f80655d09ee0acc589cbd32
KNH <nabing39701@naver.com>
2025-06-11 13:12:51 +0900

    home 수정중

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const getUserById = async (userId) => {
+  try {
+    const token = localStorage.getItem("firebaseToken"); // Firebase ID token
+    const res = await axios.get(`/api/users/${userId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return { success: true, user: res.data };
+  } catch (error) {
+    console.error("유저 조회 에러", error);
+    return { success: false };
+  }
+};
