d319c65f283ca28499da5ade677ea63493d24040
smmrue <psjung0826@naver.com>
2025-07-28 23:37:23 +0900

    수정

--- frontend/src/pages/postDetails.jsx
+++ frontend/src/pages/postDetails.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import PostCard from "../components/PostCard";
 import { auth } from "../api/firebase";
 
 const PostDetails = () => {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const postId = searchParams.get("postId");
 
   const [post, setPost] = useState(null);
@@ -18,26 +19,34 @@ const PostDetails = () => {
     const fetchPost = async () => {
       try {
         const res = await axios.get(`/api/posts/${postId}`);
-        if (res.data.success) {
-          const data = res.data.data;
 
-          if (!data.user) {
-            data.user = {
-              name: "User",
-              image: "/defaultUser.png",
-            };
-          }
-          setPost(data);
+        if (res.data.success) {
+        const postData = res.data.data;
+
+        // 🔹 user 데이터가 없으면 기본값 채우기
+        if (!postData.user) {
+          postData.user = {
+            name: "User",
+            image: "/defaultUser.png",
+          };
+        }
+        setPost(postData);
         } else {
-          setError("게시글을 불러올 수 없습니다.");
+          setError(res.data.message || "게시글을 불러올 수 없습니다.");
+          navigate("/home"); 
         }
       } catch (err) {
+        if (err.response?.status === 404) {
+        setError("삭제되었거나 존재하지 않는 게시글입니다.");
+      } else {
         setError("에러 발생: " + err.message);
       }
+      navigate("/home");
+    } 
     };
 
-    if (postId) fetchPost();
-  }, [postId]);
+    fetchPost();
+  }, [postId, navigate]);
 
   if (error) return <div>로딩 중...</div>;
   if (!post) return <div>{error}</div>;
