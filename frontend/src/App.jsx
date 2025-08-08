ed310f382cc4e7dea8beafa04ca683ec5dfc3764
KNH <nabing39701@naver.com>
2025-08-08 16:00:09 +0900

    editprofile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import SearchPage from "./pages/SearchPage";
 import NewPost from "./pages/newPost";
 import NotificationPage from "./pages/NotificationPage";
 import Profile from "./pages/profile";
-import EditProfile from "./pages/editProfile";
+import EditProfile from "./pages/EditProfile";
 import EditMosaic from "./pages/editMosaic";
 import PostDetails from "./pages/postDetails";
 
@@ -15,19 +15,21 @@ function App() {
   return (
     <FilesProvider>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/SearchPage" element={<SearchPage />} />
-        <Route path="/UploadPage" element={<NewPost />} />
-        <Route path="/editMosaic" element={<EditMosaic />} />
-        <Route path="/notifications" element={<NotificationPage />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/editprofile" element={<EditProfile />} />
-        <Route path="/postDetail" element={<PostDetails />} />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/SearchPage" element={<SearchPage />} />
+          <Route path="/UploadPage" element={<NewPost />} />
+          <Route path="/editMosaic" element={<EditMosaic />} />
+          <Route path="/notifications" element={<NotificationPage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:id" element={<Profile />} />{" "}
+          {/* 다른 유저 프로필 */}
+          <Route path="/EditProfile" element={<EditProfile />} />
+          <Route path="/postDetail" element={<PostDetails />} />
+        </Routes>
+      </BrowserRouter>
     </FilesProvider>
   );
 }
